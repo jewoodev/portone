8571743cb4981ad0983c10c5acaa4406193c1819
jewoodev <jewoos15@naver.com>
2024-12-16 15:25:07 +0900

    📝 update: 회원가입 검증 로직 분리

--- src/main/java/com/portone/web/controller/MemberController.java
+++ src/main/java/com/portone/web/controller/MemberController.java
@@ -4,6 +4,7 @@ import com.portone.domain.common.Role;
 import com.portone.domain.dto.LoginDto;
 import com.portone.domain.dto.SignupDto;
 import com.portone.domain.entity.Member;
+import com.portone.web.controller.validator.SignUpValidator;
 import com.portone.web.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -28,6 +29,7 @@ import java.util.UUID;
 public class MemberController {
     private final MemberService memberService;
     private final BCryptPasswordEncoder passwordEncoder;
+    private final SignUpValidator signUpValidator;
 
     @GetMapping("/signup")
     public String signupForm(Model model) {
@@ -38,40 +40,23 @@ public class MemberController {
     @PostMapping("/signup")
     public String signup(
             @Validated @ModelAttribute SignupDto signupDto,
-            BindingResult result,
-            Errors errors
+            BindingResult result
     ) {
-
         if (result.hasErrors())
             return "members/signup"; // 문제가 있는 경우는 다시 signup page
+        signUpValidator.validate(signupDto, result);
 
-        // 1차, 2차 비밀번호 검증
-        String password1 = signupDto.getPassword1();
-        String password2 = signupDto.getPassword2();
-        if (!password1.equals(password2)) {
-            result.rejectValue("password2", "passwordIncorrect",
-                    "2개의 패스워드가 일치하지 않습니다.");
-            return "members/signup";
-        }
 
-        // 아이디 중복 검증
-        String username = signupDto.getUsername();
-        try {
-            memberService.existByUsername(username);
-        } catch (IllegalStateException | InvalidDataAccessApiUsageException e) {
-            result.rejectValue("username", "duplicatedUsername", e.getMessage());
-            return "members/signup";
-        }
-
-        String encodedPwd = passwordEncoder.encode(password1);
+        String encodedPwd = passwordEncoder.encode(signupDto.getPassword1());
         Member member = Member.builder()
                 .id(UUID.randomUUID().toString())
-                .username(username)
+                .username(signupDto.getUsername())
                 .password(encodedPwd)
                 .role(Role.USER)
                 .createDate(LocalDateTime.now())
                 .modifiedDate(LocalDateTime.now())
                 .build();
+
         memberService.signup(member);
         return "redirect:/login";
     }
