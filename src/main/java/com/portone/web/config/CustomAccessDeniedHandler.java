45c76a93ffc978768216e866d97bbc7b53a16ca8
jewoodev <jewoos15@naver.com>
2024-12-19 09:23:12 +0900

    🚑 fix: AJAX 요청 시 권한 문제 응답 타입 문제 해결

--- src/main/java/com/portone/web/config/CustomAccessDeniedHandler.java
+++ src/main/java/com/portone/web/config/CustomAccessDeniedHandler.java
@@ -14,11 +14,18 @@ import java.io.PrintWriter;
 public class CustomAccessDeniedHandler  implements AccessDeniedHandler {
     @Override
     public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
-        response.setContentType("text/html;charset=UTF-8");
-        PrintWriter out = response.getWriter();
-        out.println("<script>");
-        out.println("alert('권한이 없습니다. 관리자에게 문의해주세요.');");
-        out.println("window.location.href = '/';"); // 메인 페이지 또는 다른 경로로 리다이렉트
-        out.println("</script>");
+        String requestedWith = request.getHeader("X-Requested-With");
+        if ("XMLHttpRequest".equals(requestedWith)) {
+            response.setContentType("application/json;charset=UTF-8");
+            response.setStatus(HttpServletResponse.SC_FORBIDDEN); // 403 상태 코드 설정
+            response.getWriter().write("{\"status\":\"error\", \"message\":\"로그인이 필요합니다.\"}");
+        } else {
+            response.setContentType("text/html;charset=UTF-8");
+            PrintWriter out = response.getWriter();
+            out.println("<script>");
+            out.println("alert('권한이 없습니다. 관리자에게 문의해주세요.');");
+            out.println("window.location.href = '/';"); // 메인 페이지 또는 다른 경로로 리다이렉트
+            out.println("</script>");
+        }
     }
 }
--- src/main/java/com/portone/web/config/CustomAuthenticationEntryPoint.java
+++ src/main/java/com/portone/web/config/CustomAuthenticationEntryPoint.java
@@ -14,11 +14,19 @@ import java.io.PrintWriter;
 public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
-        response.setContentType("text/html;charset=UTF-8");
-        PrintWriter out = response.getWriter();
-        out.println("<script>");
-        out.println("alert('로그인이 필요한 기능입니다.');");
-        out.println("window.location.href = '/login';"); //로그인 페이지로 이동
-        out.println("</script>");
+        // AJAX 요청인 경우 JSON으로 반환
+        String requestedWith = request.getHeader("X-Requested-With");
+        if ("XMLHttpRequest".equals(requestedWith)) {
+            response.setContentType("application/json;charset=UTF-8");
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED); // 403 상태 코드 설정
+            response.getWriter().write("{\"status\":\"error\", \"message\":\"로그인이 필요합니다.\"}");
+        } else {
+            response.setContentType("text/html;charset=UTF-8");
+            PrintWriter out = response.getWriter();
+            out.println("<script>");
+            out.println("alert('로그인이 필요한 기능입니다.');");
+            out.println("window.location.href = '/login';"); //로그인 페이지로 이동
+            out.println("</script>");
+        }
     }
 }
