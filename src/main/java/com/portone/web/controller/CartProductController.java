1dbe2b700ce3c00225ff1b4df72785b858f4e39b
jewoodev <jewoos15@naver.com>
2024-12-19 14:36:04 +0900

    📰 feat: 장바구니 업데이트 기능

---
+++ src/main/java/com/portone/web/controller/CartProductController.java
@@ -0,0 +1,76 @@
+package com.portone.web.controller;
+
+import com.portone.domain.dto.CustomUserDetails;
+import com.portone.domain.entity.CartProduct;
+import com.portone.web.service.CartProductService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.NoSuchElementException;
+
+@Slf4j
+@RequiredArgsConstructor
+@Controller
+public class CartProductController {
+    private final CartProductService cartProductService;
+
+    @ResponseBody
+    @PostMapping("/api/cart/add")
+    public Map<String, Object> addToCart(@RequestBody Map<String, String> payload,
+                                         @AuthenticationPrincipal CustomUserDetails customUserDetails
+    ) {
+        Map<String, Object> response = new HashMap<>();
+        try {
+            String productName = payload.get("productName");
+            cartProductService.addToCart(customUserDetails.getMember().getUid(), productName);
+        } catch (NoSuchElementException e) {
+            e.printStackTrace();
+            log.info(e.getMessage());
+            response.put("status", "error");
+            response.put("message", e.getMessage());
+        }
+        response.put("status", "success");
+        return response;
+    }
+
+    @GetMapping("/cart/{uid}")
+    public String cartView(@PathVariable String uid, Model model) {
+        List<CartProduct> cartProductList = cartProductService.findCartProduct(uid);
+        model.addAttribute("cartProductList", cartProductList);
+        return "product/cart";
+    }
+
+    @PostMapping("/cart/update")
+    public String cartUpdate(@RequestParam Map<String, String> quantities,
+                             @RequestParam(required = false) List<String> deleteUids,
+                             @AuthenticationPrincipal CustomUserDetails customUserDetails
+    ) {
+        // CSRF 토큰 제거
+        quantities.remove("_csrf");
+        quantities.remove("deleteUids");
+
+        // 1. 삭제 처리
+        if (deleteUids != null && !deleteUids.isEmpty()) {
+            cartProductService.deleteByUids(deleteUids);
+            for (String deleteUid : deleteUids) {
+                quantities.remove(deleteUid);
+            }
+        }
+
+        // 2. 수량 업데이트 처리
+        if (quantities != null && !quantities.isEmpty()) {
+            cartProductService.updateQuantityMultiple(quantities);
+        }
+
+        String memberUid = customUserDetails.getUid();
+
+        return "redirect:/cart/" + memberUid;
+    }
+}
---
+++ src/main/java/com/portone/web/service/CartProductService.java
@@ -0,0 +1,63 @@
+package com.portone.web.service;
+
+import com.portone.domain.entity.CartProduct;
+import com.portone.domain.repository.CartProductRepository;
+import com.portone.domain.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.*;
+
+@RequiredArgsConstructor
+@Service
+public class CartProductService {
+    private final CartProductRepository cartProductRepository;
+    private final ProductRepository productRepository;
+
+    @Transactional
+    public String addToCart(String memberUid, String productName) {
+        // 상품 검증
+        productRepository.findByName(productName).orElseThrow(() -> new NoSuchElementException("존재하지 않는 옷입니다."));
+
+        Optional<CartProduct> opCartProduct = cartProductRepository.findByMemberUidAndProductName(memberUid, productName);
+        String cartProductUid;
+        if (opCartProduct.isEmpty()) {
+            CartProduct cartProduct = CartProduct.builder()
+                    .uid(UUID.randomUUID().toString())
+                    .productName(productName)
+                    .memberUid(memberUid)
+                    .quantity(1)
+                    .build();
+
+            cartProductUid = cartProductRepository.save(cartProduct).getUid();
+        }
+        else {
+            CartProduct cartProduct = opCartProduct.get();
+            cartProduct.increaseQuantity();
+            cartProductUid = cartProduct.getUid();
+        }
+
+        return cartProductUid;
+    }
+
+    @Transactional(readOnly = true)
+    public List<CartProduct> findCartProduct(String MemberUid) {
+        List<CartProduct> cartProductList = cartProductRepository.findByMemberUidOrderByProductNameAsc(MemberUid);
+        return cartProductList;
+    }
+
+    @Transactional
+    public void deleteByUids(List<String> cartProductUids) {
+        for (String cartProductUid : cartProductUids) {
+            cartProductRepository.deleteByUid(cartProductUid);
+        }
+    }
+
+    @Transactional
+    public void updateQuantityMultiple(Map<String, String> quantities) {
+        for (Map.Entry<String, String> entry : quantities.entrySet()) {
+            cartProductRepository.updateQuantityByUid(entry.getKey(), Integer.parseInt(entry.getValue()));
+        }
+    }
+}
